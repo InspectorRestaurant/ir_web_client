4f66d2b596e5c27de1dc80aa787bd127d4aeeab7
Stephanie Tan <tans2@rpi.edu>
2018-04-25 13:40:36 -0400

    About Us page fixed with added descriptions

--- src/containers/main_about/index.vue
+++ src/containers/main_about/index.vue
@@ -8,7 +8,7 @@
 
 <!-- seems like my-5 is the threshold -->
           <div class="col-lg-4 my-5">
-            <p>We are a student group creating this web application to call forth attention to food safety, and offering consumers a online information source easy to use to learn about food inspection histories.</p>
+            <p>We are a student group creating this web application to call forth attention to food safety, and offering consumers an easy to use online information source to learn about food inspection history.</p>
           </div>
 
           <div class="col-lg-8 text-center">
@@ -53,31 +53,29 @@
           </div>
         </div>
 
-        <TeamMember name="Alex"/>
+        <TeamMember name="Alex" bio="Alex is an Computer Science major graduating in the class of 2018. He has multitudes of experience from working at Cisco as well as a passion for coding. He has been the technical lead for this project and has taught and guided the team throughout. "/>
 
         <div class="row col-lg-12 align-items-center mb-4">
           <button class="btn btn-primary bg-light"><a href="#2">next</a></button>
         </div>
 
-        <TeamMember  name="Jonathan"/>
+        <TeamMember  name="Jonathan" bio="Jonathan is a Computer Science and ITWS dual major concentrating in Web Technologies. His CS background and internship experiences make him a effective and efficient addition to the team in all forms of development. "/>
 
         <div class="row col-lg-12 align-items-center mb-4">
           <button class="btn btn-primary bg-light"><a href="#3">next</a></button>
         </div>
 
-        <TeamMember name="Diana" />
+        <TeamMember name="Diana" id="Diana"/>
 
         <div class="row col-lg-12 align-items-center mb-4">
           <button class="btn btn-primary bg-light"><a href="#4">next</a></button>
         </div>
 
-        <TeamMember  name="Kay" />
+        <TeamMember  name="Stephanie" bio="Stephanie is an ITWS student concentrating in Data Science and Communications graduating in the Class of 2019. She is also pursuing minors in Computer Science and Chinese. With internship experience in Information Security, coursework in both Computer Science and ITWS, Stephanie is a strong contributor to the team in fullstack development."/>
 
         <div class="row col-lg-12 align-items-center mb-4">
           <button class="btn btn-primary bg-light"><a href="#5">next</a></button>
         </div>
-
-        <TeamMember  name="Stephanie" />
 
   </div>
 
