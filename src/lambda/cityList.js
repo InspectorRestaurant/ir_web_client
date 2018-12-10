fd32c588e1280e9846a4e5c578df2c5685415759
Alexander Schwartzberg <aeksco@gmail.com>
2018-12-10 10:31:16 -0500

    Simplified Vue configuration, refined citylist lambda

--- src/lambda/cityList.js
+++ src/lambda/cityList.js
@@ -24,26 +24,67 @@ function successResponse(callback, res) {
 
 // // // //
 
+// // GET /api/restaurants
+// export function handler(event, context, callback) {
+//   MongoClient.connect(DB_URL, (err, connection) => {
+//     if (err) return errorResponse(callback, err);
+
+//     const db = connection.db(DB_NAME);
+//     const restaurantCollection = db.collection('restaurants');
+
+//     // GET /api/cities
+//     restaurantCollection.distinct('address.city', (err, result) => {
+//         if (err) return errorResponse(callback, err);
+
+//         // res.setHeader('Cache-Control', 'max-age=604800, public');
+//         callback(null, {
+//           statusCode: 200,
+//           body: JSON.stringify(result)
+//         });
+
+//     })
+
+//   });
+
+// }
+
+
+// // // //
+
 // GET /api/restaurants
 export function handler(event, context, callback) {
-  MongoClient.connect(DB_URL, (err, connection) => {
+
+  // Connects to MongoDB
+  MongoClient.connect(DB_URL, { useNewUrlParser: true }, (err, connection) => {
+
+    // Connection error handling
     if (err) return errorResponse(callback, err);
 
+    // DB helpers
     const db = connection.db(DB_NAME);
-    const restaurantCollection = db.collection('restaurants');
+    const restaurantCollection = db.collection('restaurants'); // TODO - constantize 'restaurants'
 
-    // GET /api/cities
+    // Pulls pagination parameters
+    const { page, per_page, skip } = handlePagination(event.queryStringParameters || {})
+
+    // Gets distinct cities
     restaurantCollection.distinct('address.city', (err, result) => {
-        if (err) return errorResponse(callback, err);
 
-        // res.setHeader('Cache-Control', 'max-age=604800, public');
-        callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(result)
-        });
+          // Handles find query error
+          if (err) return errorResponse(callback, err);
+
+          // Closes the DB connection
+          connection.close();
+
+          // Sends response to client
+          callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(result)
+          });
+
+        })
 
     })
 
   });
-
 }
